147bc38fb76c84c427246f6e4a3f8a109f122133
ITX351 <itx351@126.com>
2016-05-23 21:44:39 +0800

    Diagnose Conclusion Items added into transaction.

--- orabs/Diagnose/frmMeetingDiagnose.cs
+++ orabs/Diagnose/frmMeetingDiagnose.cs
@@ -53,11 +53,46 @@ namespace orabs.Meeting
             {
                 string exeStr = "insert into DiagnoseConclusion(Meeting_ID, Symptom, Conclusion, Handle, Cost, Paid) values(" +
                     Meeting_ID.ToString() + ", '" + txtSymptom.Text + "', '" + txtConclusion.Text + "', '" +
-                    txtHandle.Text + "', 0, 0)"; // TODO: Cost calculating
+                    txtHandle.Text + "', " + TotalPrice.ToString() + ", 0)";
                 DatabaseOperation.ExecuteSQLQuery(exeStr, transaction);
                 exeStr = "update Meeting set Status = 'F' where Meeting_ID = " + Meeting_ID.ToString();
                 DatabaseOperation.ExecuteSQLQuery(exeStr, transaction);
 
+                if (TotalPrice > 0)
+                {
+                    // Get User_ID and Patient_ID;
+                    string queryStr = "select User_ID, Patient.Patient_ID from Meeting join Patient on Patient.Patient_ID = Meeting.Patient_ID" +
+                        " join User on Patient.Patient_ID = User.Patient_ID where Meeting.Meeting_ID = " + Meeting_ID.ToString();
+                    DataTable dt = DatabaseOperation.GetDataTableByQuery(queryStr);
+                    int User_ID = (int)dt.Rows[0]["User_ID"], Patient_ID = (int)dt.Rows[0]["Patient_ID"];
+
+                    // Insert PaymentList
+                    exeStr = "insert into PaymentList(User_ID, Patient_ID, Doctor_ID, Meeting_ID, Totalprice, Paid) values" + 
+                        "(" + User_ID.ToString() + ", " + Patient_ID.ToString() + ", " + Global.doctorId.ToString() + ", " +
+                        Meeting_ID.ToString() + ", " + TotalPrice.ToString() + ", 0)";
+                    DatabaseOperation.ExecuteSQLQuery(exeStr, transaction);
+
+                    // Get PaymentList_ID
+                    queryStr = "select PaymentList_ID from PaymentList where Meeting_ID = " + Meeting_ID.ToString();
+                    dt = DatabaseOperation.GetDataTableByQuery(queryStr);
+                    int PaymentList_ID = (int)dt.Rows[0]["PaymentList_ID"];
+
+                    // Insert PaymentList_PurchaseItem
+                    exeStr = "insert into PaymentList_PurchaseItem(PurchaseItem_ID, PaymentList_ID, Number) values";
+
+                    bool NotFirstInForeach = false;
+                    foreach (DataRow dr in chosenItems.Rows)
+                    {
+                        if (NotFirstInForeach)
+                            exeStr += ",";
+                        NotFirstInForeach = true;
+
+                        exeStr += "(" + dr["PurchaseItem_ID"].ToString() + ", " + PaymentList_ID.ToString() + ", " + 
+                            dr["Number"].ToString() + ")";
+                    }
+                    DatabaseOperation.ExecuteSQLQuery(exeStr, transaction);
+
+                }
                 transaction.Commit();
             }
             catch (Exception exception)
